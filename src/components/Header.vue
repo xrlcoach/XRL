f9c0df7a7ed8319dd72db3c0c6ac58ffc55e6b7d
jamesedchristie <jamesedchristie1@gmail.com>
2025-02-22 16:15:27 +1100

    Update header to XRL 25

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,47 +1,52 @@
 <template>
   <div class="headerContainer">
-    <div id="headerOffset" v-if="!isMobile">
-      &nbsp;
-    </div>
-    <h1>XRL '24</h1>    
+    <div id="headerOffset" v-if="!isMobile">&nbsp;</div>
+    <h1>XRL '25</h1>
     <div id="headerActions">
-      <Button label="Refresh Data" @click="refresh" class="p-button-text" icon="pi pi-refresh" loadingIcon="pi pi-spin pi-spinner" :loading="loading" />
+      <Button
+        label="Refresh Data"
+        @click="refresh"
+        class="p-button-text"
+        icon="pi pi-refresh"
+        loadingIcon="pi pi-spin pi-spinner"
+        :loading="loading"
+      />
       <Button label="Logout" @click="logout" class="p-button-text p-button-danger" />
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, ref } from 'vue'
-import { useRoute, useRouter } from 'vue-router'
-import { LogoutUser } from '../services/auth'
-import { RefreshSession } from '../services/utils';
-import { useXrlStore } from '../store';
+  import { computed, defineComponent, ref } from 'vue';
+  import { useRoute, useRouter } from 'vue-router';
+  import { LogoutUser } from '../services/auth';
+  import { RefreshSession } from '../services/utils';
+  import { useXrlStore } from '../store';
 
-export default defineComponent({
-  setup() {
-    const router = useRouter();
-    const store = useXrlStore();
-    const isMobile = computed(() => store.state.isMobile);
-    const loading = ref(false);
-    const logout = () => {
-      LogoutUser();
-      router.replace({ name: 'Login' });
-    };
-    const refresh = async () => {
-      loading.value = true;
-      await RefreshSession();
-      loading.value = false;
-      window.location.reload();
-    }
-    return {
-      logout,
-      refresh,
-      loading,
-      isMobile
-    }
-  },
-})
+  export default defineComponent({
+    setup() {
+      const router = useRouter();
+      const store = useXrlStore();
+      const isMobile = computed(() => store.state.isMobile);
+      const loading = ref(false);
+      const logout = () => {
+        LogoutUser();
+        router.replace({ name: 'Login' });
+      };
+      const refresh = async () => {
+        loading.value = true;
+        await RefreshSession();
+        loading.value = false;
+        window.location.reload();
+      };
+      return {
+        logout,
+        refresh,
+        loading,
+        isMobile,
+      };
+    },
+  });
 </script>
 
 <style scoped>
@@ -52,7 +57,8 @@ export default defineComponent({
     width: 100%;
     margin-bottom: 20px;
   }
-  #headerOffset, #headerActions {
+  #headerOffset,
+  #headerActions {
     flex: 0 1 250px;
   }
   h1 {
